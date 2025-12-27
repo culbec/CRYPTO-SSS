43a9ddf7c16a6db2c6c08edd3d7deb0ebc45a4ea
Alexandru PROFIR <aprofir@bitdefender.com>
2025-12-28 01:51:10 +0200

    culbec: initialized Go backend.

---
+++ src/backend/utils/strings/strings.go
@@ -0,0 +1,16 @@
+package strings
+
+
+func Echo(s string) string {
+    return s
+}
+
+func Reverse(s string) string {
+    rev := []rune(s)
+    
+    for i, j := 0, len(rev) - 1; i < j; i, j = i + 1, j - 1 {
+        rev[i], rev[j] = rev[j], rev[i]
+    }
+    
+    return string(rev)
+}
